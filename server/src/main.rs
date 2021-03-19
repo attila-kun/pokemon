01757c23f421449755d2c109e76983bf83cf1367
Attila Kun <>
2021-03-19 18:25:58 +0000

    Added actix_web.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,18 @@
+use actix_web::{App, HttpResponse, HttpServer, Result, get, web};
 use pokemon_core;
 
-fn main() {
-    pokemon_core::hello();
+#[get("/pokemon/{name}")]
+async fn pokemon(info: web::Path<(String)>) -> HttpResponse {
+    println!("{}", &info.0);
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body("something")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| { App::new().service(pokemon) })
+    .bind("0.0.0.0:5000")?
+    .run()
+    .await
 }

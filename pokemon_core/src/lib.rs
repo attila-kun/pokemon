959e9be60a3a338ffb9184b5f4c8f0e86406eae0
Attila Kun <>
2021-03-19 18:53:15 +0000

    Returned mock JSON response.

--- pokemon_core/src/lib.rs
+++ pokemon_core/src/lib.rs
@@ -1,3 +1,3 @@
-pub fn hello() {
-    println!("hello");
+pub fn get_shakespearean_description(pokemon_name: String) -> String {
+    String::from(pokemon_name + " description")
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,20 @@
-use actix_web::{App, HttpResponse, HttpServer, Result, get, web};
-use pokemon_core;
+use actix_web::{App, HttpServer, Result, get, web};
+use pokemon_core::get_shakespearean_description;
+use serde::{Serialize};
+
+#[derive(Serialize)]
+struct PokemonResponse {
+    name: String,
+    description: String
+}
 
 #[get("/pokemon/{name}")]
-async fn pokemon(info: web::Path<(String)>) -> HttpResponse {
-    println!("{}", &info.0);
-    HttpResponse::Ok()
-        .content_type("application/json")
-        .body("something")
+async fn pokemon(info: web::Path<(String)>) -> Result<web::Json<PokemonResponse>> {
+    let pokemon_name = &info.0;
+    Ok(web::Json(PokemonResponse {
+        name: String::from(pokemon_name),
+        description: get_shakespearean_description(String::from(pokemon_name))
+    }))
 }
 
 #[actix_web::main]

b3bf4f758ffa42c58e94d025efb3c5cf99a89c79
Attila Kun <>
2021-03-20 02:55:34 +0000

    Collected ability descriptions in parallel.

--- pokemon_core/src/lib.rs
+++ pokemon_core/src/lib.rs
@@ -1,13 +1,21 @@
 mod ability_urls;
 mod ability_description;
 mod json_request;
+use futures::future::join_all;
 
 pub async fn get_shakespearean_description(pokemon_name: &str) -> Result<String, ()> {
     let pokemon_ability_urls_result = ability_urls::get_pokemon_ability_urls(pokemon_name).await;
     match pokemon_ability_urls_result {
         Ok(urls) => {
-            let url = urls.get(0).unwrap();
-            println!("{:?}", ability_description::get_ability_description(url).await);
+            let futures = urls.iter().map(|url| { ability_description::get_ability_description(url) });
+            let ability_description_results = join_all(futures).await;
+            let ability_descriptions: Vec<String> = ability_description_results
+                .into_iter()
+                .filter(|result| { result.is_ok() })
+                .map(|result| { result.unwrap() })
+                .collect();
+
+            println!("{:?}", ability_descriptions);
             todo!()
         },
         Err(()) => Err(())
--- pokemon_core/tests/integration_test.rs
+++ pokemon_core/tests/integration_test.rs
@@ -2,5 +2,5 @@ use pokemon_core::get_shakespearean_description;
 
 #[actix_rt::test]
 async fn test_index_get() {
-    println!("{:?}", get_shakespearean_description("pikachu").await);
+    println!("{:?}", get_shakespearean_description("charizard").await);
 }

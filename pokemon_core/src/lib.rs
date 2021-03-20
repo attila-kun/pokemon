266f64b730fb0741ee74e58fedb9f52cd83619ae
Attila Kun <>
2021-03-20 01:08:28 +0000

    Printed errors.

--- pokemon_core/src/lib.rs
+++ pokemon_core/src/lib.rs
@@ -18,14 +18,17 @@ struct PokemonJson {
     abilities: Vec<PokemonAbilityItem>
 }
 
-async fn get_pokemon_ability_urls(pokemon_name: &str) -> Result<Vec<String>, String> {
+async fn get_pokemon_ability_urls(pokemon_name: &str) -> Result<Vec<String>, ()> {
 
     let api_base_url = url::Url::parse("https://pokeapi.co/api/v2/pokemon/").unwrap();
     let api_url_result = api_base_url.join(pokemon_name);
 
     let api_url = match api_url_result {
         Ok(url) => url.to_string(),
-        Err(_) => return Err(String::from("Error creating API URL."))
+        Err(_) => {
+            eprint!("Error creating API URL for Pokemon: {}", pokemon_name);
+            return Err(());
+        }
     };
 
     let mut client = Client::default();
@@ -36,17 +39,26 @@ async fn get_pokemon_ability_urls(pokemon_name: &str) -> Result<Vec<String>, Str
 
     let body_response = match initial_response {
         Ok(mut response) => response.body().await,
-        Err(_) => return Err(String::from("Error requesting Pokemon data."))
+        Err(_) => {
+            eprint!("Error requesting Pokemon data for Pokemon: {}", pokemon_name);
+            return Err(());
+        }
     };
 
     let body_utf8_parse_result = match body_response {
         Ok(body_response_bytes) => String::from_utf8(body_response_bytes.to_vec()),
-        Err(_) => return Err(String::from("Error while receiving Pokemon data body."))
+        Err(_) => {
+            eprint!("Error while receiving Pokemon data body for Pokemon: {}", pokemon_name);
+            return Err(());
+        }
     };
 
     let pokemon_json_parse_result: serde_json::Result<PokemonJson> = match body_utf8_parse_result {
         Ok(body_utf8_string) => serde_json::from_str(&body_utf8_string),
-        Err(_) => return Err(String::from("Error while parsing body into UTF8 string."))
+        Err(_) => {
+            eprint!("Error while parsing body into UTF8 string for Pokemon: {}", pokemon_name);
+            return Err(());
+        }
     };
 
     match pokemon_json_parse_result {
@@ -56,12 +68,17 @@ async fn get_pokemon_ability_urls(pokemon_name: &str) -> Result<Vec<String>, Str
                 .map(|ability_item| { ability_item.ability.url.clone() })
                 .collect())
         },
-        Err(error) => Err(error.to_string())
+        Err(error) => {
+            eprint!("Error while parsing UTF8 string into JSON for Pokemon {}. Error: {}", pokemon_name, error.to_string());
+            return Err(());
+        }
     }
 }
 
-pub async fn get_shakespearean_description(pokemon_name: &str) -> String {
-    let result = get_pokemon_ability_urls(pokemon_name).await;
-    println!("result {:?}", result);
-    String::new()
+pub async fn get_shakespearean_description(pokemon_name: &str) -> Result<String, ()> {
+    let pokemon_ability_urls_result = get_pokemon_ability_urls(pokemon_name).await;
+    match pokemon_ability_urls_result {
+        Ok(urls) => todo!(),
+        Err(()) => Err(())
+    }
 }
